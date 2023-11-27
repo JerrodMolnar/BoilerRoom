68697884d62d62fc727d90cfa5353a62f09c0a64
Jerrod Molnar <Jerrod.Molnar@gmail.com>
2023-11-26 22:40:52 -0500

    Levers fixed Added Fire

--- Assets/Scripts/Lights/FireLight.cs
+++ Assets/Scripts/Lights/FireLight.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FireLight : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 1f; 
+    private Light _light;
+    private float _lightIntensityMinimum = 20f, _lightIntensityMaximum = 100f;
+    private float _lightRangeMinimum = 10f, _lightRangeMaximum = 30f;
+    private int _positiveIntensity = 1;
+    private int _positiveRange = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _light = GetComponent<Light>();
+        if (_light == null)
+        {
+            Debug.LogError("Light is null on FireLight");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        _light.intensity += _speed * _positiveIntensity;
+        _light.range += _speed * _positiveRange;
+
+        if (_light.intensity < _lightIntensityMinimum || _light.intensity > _lightIntensityMaximum)
+        {
+            _positiveIntensity = -_positiveIntensity;
+        }
+
+        if (_light.range < _lightRangeMinimum || _light.range > _lightRangeMaximum)
+        { 
+            _positiveRange = -_positiveRange; 
+        }
     }
 }
