45ab6206bc3f5a7b41d54faf4fdeb317f52a313c
Jerrod Molnar <Jerrod.Molnar@gmail.com>
2023-12-01 21:02:34 -0500

    Still fixing bugs
    Working on physics of the gold key.

--- Assets/Scripts/DrawerCloser.cs
+++ Assets/Scripts/DrawerCloser.cs
@@ -2,17 +2,37 @@ using UnityEngine;
 
 public class DrawerCloser : MonoBehaviour
 {
+    private bool _drawerActivate = false;
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        _startPosition = transform.localPosition;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.localPosition.z < 0)
+        if (_drawerActivate)
         {
-            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 0);
+            if (transform.localPosition.z < 0)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 0);
+            }
+            else if (transform.localPosition.z > .3f)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 0.3f);
+            }
         }
-        else if (transform.localPosition.z > .3f)
+        else
         {
-            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 0.3f);
+            transform.localPosition = _startPosition;
         }
+
+    }
+
+    public void GrabDrawer()
+    {
+        _drawerActivate = true;
     }
 }
