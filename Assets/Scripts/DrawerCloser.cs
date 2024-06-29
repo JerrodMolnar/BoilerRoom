df875f8b22681f9576a890f71fc82ba301057074
Jerrod Molnar <Jerrod.Molnar@gmail.com>
2024-06-28 22:52:01 -0400

    Fixing Things
    Trying to get things working smoother and more efficient

--- Assets/Scripts/DrawerCloser.cs
+++ Assets/Scripts/DrawerCloser.cs
@@ -3,11 +3,24 @@ using UnityEngine;
 public class DrawerCloser : MonoBehaviour
 {
     private bool _drawerActivate = false;
-    private Vector3 _startPosition;
+    private Vector3 _startPosition, _endPosition, _handleStartPosition, _handleEndPosition;
+    private Transform _handleTransform;
+
 
     private void Start()
     {
         _startPosition = transform.localPosition;
+        _endPosition = new Vector3(_startPosition.x, _startPosition.y, _startPosition.z + 0.3f);
+        _handleTransform = transform.GetComponentInChildren<Transform>();
+        if (_handleTransform != null)
+        {
+            _handleEndPosition = new Vector3(_handleStartPosition.x, _handleStartPosition.y, _handleStartPosition.z + 0.3f);
+            _handleStartPosition = _handleTransform.localPosition;
+        }
+        else
+        {
+            Debug.LogError("Transform of handle not found on " + gameObject.name);
+        }
     }
 
     public void GrabDrawer()
@@ -15,13 +28,13 @@ public class DrawerCloser : MonoBehaviour
         _drawerActivate = !_drawerActivate;
         if (_drawerActivate)
         {
-            transform.localPosition = new Vector3(_startPosition.x, _startPosition.y, _startPosition.z + 0.3f);
-            transform.GetComponentInChildren<Transform>().localPosition = new Vector3(_startPosition.x, _startPosition.y, _startPosition.z + 0.3f);
+            transform.localPosition = _endPosition;
+            _handleTransform.localPosition = _endPosition;
         }
         else
         {
             transform.localPosition = _startPosition;
-            transform.GetComponentInChildren<Transform>().localPosition = _startPosition;
+            _handleTransform.localPosition = _handleStartPosition;
         }
     }
 }
